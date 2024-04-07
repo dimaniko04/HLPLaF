ea15b1c0f7ca88304a72db44d0d15cc2b29ac67f
dimaniko04 <dimanikora04@gmail.com>
2024-04-07 11:33:10 +0300

    Implemented user authentication, login and register.

--- Lab1/articles/urls.py
+++ Lab1/articles/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create_article, name='create_article'),
     path('edit/<int:article_id>/', views.edit_article, name='edit_article'),
-    path('delete/<int:article_id>/', views.remove_article, name='remove_article')
+    path('delete/<int:article_id>/', views.remove_article, name='remove_article'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login')
 ]
--- Lab1/articles/views.py
+++ Lab1/articles/views.py
@@ -1,35 +1,70 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 
 from articles.forms import ArticleForm
 from articles.models import Article
 
 
+@login_required
 def index(request):
     articles = Article.objects.all()
 
-    return render(request, 'index.html', {'articles': articles})
+    return render(request, 'articles/index.html', {'articles': articles})
 
 
+@login_required
 def create_article(request):
     form = ArticleForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
         form.save()
         return redirect('articles:index')
-    return render(request, 'article_form.html', {'form': form})
+    return render(request, 'articles/article_form.html', {'form': form})
 
 
+@login_required
 def edit_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     form = ArticleForm(request.POST or None, instance=article)
     if request.method == 'POST' and form.is_valid():
         form.save()
         return redirect('articles:index')
-    return render(request, 'article_form.html', {'form': form})
+    return render(request, 'articles/article_form.html', {'form': form})
 
 
+@login_required
 def remove_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     if request.method == 'POST':
         article.delete()
         return redirect('articles:index')
-    return render(request, 'remove_article.html', {"article": article})
+    return render(request, 'articles/remove_article.html', {"article": article})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('articles:login')
+
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(request, user)
+        return redirect('articles:index')
+    return render(request, 'auth/register.html', {'form': form})
+
+
+def user_login(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = authenticate(**form.cleaned_data)
+        if user is not None:
+            login(request, user)
+            return redirect('articles:index')
+    return render(request, 'auth/login.html', {'form': form})
